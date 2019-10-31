d14fb8c4ce29e193ea848fbffcde73a9480b1fbc
kepo4ka <konstantin.bugulov@mail.ru>
2019-10-31 23:37:46 +0300

    safe

--- init.php
+++ init.php
@@ -1,5 +1,8 @@
 <?php
+require_once 'lib/safemysql.class.php';
+
 require_once 'config.php';
+
 require_once 'functions.php';
 require_once 'class/ElibraryDB.php';
 require_once 'class/ElibraryDB.php';
@@ -9,5 +12,6 @@ $pass = '';
 $db_name = 'elibrary';
 $host = 'localhost';
 
+$db = new SafeMysql(array('host' => $host, 'user' => $user, 'pass' => $pass, 'db' => $db_name, 'charset' => 'utf8'));
 
 ?>
