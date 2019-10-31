76881f169280e47cbaf04b77ee62d8d939013f9d
kepo4ka <konstantin.bugulov@mail.ru>
2019-10-31 23:37:35 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -3,7 +3,6 @@
 require 'init.php';
 
 
-
 $elibDb = new ElibraryDB();
 
 $graph_organisations = [];
@@ -14,14 +13,16 @@ foreach ($organisations_db as $org_info) {
     $organisation = [];
     $organisation['id'] = $org_info['id'];
     $organisation['title'] = $org_info['name'];
+    $organisation['city'] = $org_info['city'];
+    $organisation['country'] = $org_info['country'];
+    $organisation['id'] = $elibDb->generateOrganisationGraphId($organisation['id']);
 
     $authors = $elibDb->getOrganisationRelOrganisations($org_info['id']);
     $organisation['references'] = [];
     foreach ($authors as $author) {
         $rel_orgs = $elibDb->getAuthorOrganisations($author);
         foreach ($rel_orgs as $rel_org) {
-
-            $organisation['references'][] = $rel_org;
+            $organisation['references'][] = $elibDb->generateOrganisationGraphId($rel_org);
         }
     }
     $unique = array_unique($organisation['references']);
@@ -30,7 +31,8 @@ foreach ($organisations_db as $org_info) {
         $organisation['references'][] = $item;
     }
 
-    if ($k > 100) {
+
+    if ($k > 1000) {
         break;
     }
     $k++;
