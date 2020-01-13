be61e3c5709e3359f83fc5e71718a2d748f4e8ca
kepo4ka <konstantin.bugulov@mail.ru>
2020-01-13 21:58:26 +0300

    add redis lib

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/lib/safemysql.class.php';
 
 require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/predis/redis.php';
 
 require_once __DIR__ . '/functions.php';
 require_once __DIR__ . '/class/ElibraryDB.php';
---
+++ predis/redis.php
@@ -0,0 +1,134 @@
+<?php
+// Uset redis Hashes functions
+
+require_once(__DIR__ . '/autoload.php');
+
+try {
+    $redis = new \Predis\Client();
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+
+function redisGet($key, $diff_time = 120)
+{
+    global $redis, $redis_enabled;
+
+    if (!$redis_enabled) {
+        return false;
+    }
+
+    $current_time = time();
+
+    $is_exits = $redis->exists($key);
+
+    if (empty($is_exits)) {
+        return false;
+    }
+
+    $last_update_str = $redis->hget($key, 'time');
+
+    if (empty($last_update_str)) {
+        return false;
+    }
+
+    $last_update = strtotime($last_update_str);
+
+    if (($current_time - $last_update) < $diff_time) {
+        return false;
+    }
+
+    $value = $redis->hget($key, 'value');
+
+    if (empty($value)) {
+        return false;
+    }
+
+    $value = json_decode($value, true);
+
+    return $value;
+}
+
+function redisSet($key, $value)
+{
+    global $redis, $redis_enabled;
+    if (!$redis_enabled) {
+        return false;
+    }
+    $redis->hset($key, 'time', time());
+    $res = $redis->hset($key, 'value', json_encode($value, JSON_UNESCAPED_UNICODE));
+    return $res;
+}
+
+function redisDel($key)
+{
+    global $redis, $redis_enabled;
+
+    if (!$redis_enabled) {
+        return false;
+    }
+
+    if ($redis->exists($key)) {
+        $redis->del($key);
+    }
+    return true;
+}
+
+
+function redisDelKeys($keys)
+{
+    global $redis, $redis_enabled;
+
+    if (!$redis_enabled) {
+        return false;
+    }
+
+    if (!empty($keys)) {
+        foreach ($keys as $key) {
+            if ($redis->exists($key)) {
+                $redis->del($key);
+            }
+        }
+    }
+
+    return true;
+}
+
+
+function redisDelAll()
+{
+    global $redis, $redis_enabled;
+
+    if (!$redis_enabled) {
+        return false;
+    }
+
+    return $redis->flushdb();
+}
+
+
+function redisDelKey($key)
+{
+    global $redis, $redis_enabled;
+
+    if (!$redis_enabled) {
+        return false;
+    }
+
+    return $redis->del($key);
+}
+
+function redisDelKeyPattern($pattern)
+{
+    global $redis, $redis_enabled;
+
+    if (!$redis_enabled) {
+        return false;
+    }
+
+    $keys = $redis->keys($pattern);
+    return redisDelKeys($keys);
+}
+
+
+?>
