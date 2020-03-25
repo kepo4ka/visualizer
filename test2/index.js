b03453d463e0c6bd33d1afbc4623df09bacb2595
kepo4ka <konstantin.bugulov@mail.ru>
2020-03-25 21:27:39 +0300

    Update index.js

--- test2/index.js
+++ test2/index.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     const $data_source_handler = $('#data_source_handler');
     const $node_count_container = $('#node_count_container');
     const $node_count_input = $('#node_count_input');
+    const $graph_type_handler = $('#graph_type_handler');
+
     const $preloader = $('.transition-loader');
     const $full_preloader = $('.full_preloader');
     const $node_relations_outcoming = $('.node_relations_outcoming');
@@ -11,6 +13,7 @@ $(document).ready(function () {
 
     let data_source = localStorage.getItem('data_source') || 'flare';
     let node_length = parseInt(localStorage.getItem('node_length'));
+    let graph_type = localStorage.getItem('graph_type') || 'hierarchy';
 
     let url = '';
 
@@ -31,6 +34,17 @@ $(document).ready(function () {
     menuSetup();
 
 
+    $graph_type_handler.on('change', function () {
+        let new_type = $(this).val();
+
+        if (graph_type == new_type) {
+            return;
+        }
+        graph_type = new_type;
+        localStorage.setItem('graph_type', graph_type);
+        getDataUrl();
+    });
+
     $data_source_handler.on('change', function () {
         let new_data_source = $(this).val();
 
@@ -39,7 +53,6 @@ $(document).ready(function () {
         }
 
         data_source = new_data_source;
-
         localStorage.setItem('data_source', data_source);
         getDataUrl();
     });
@@ -52,7 +65,7 @@ $(document).ready(function () {
 
 
     function getDataUrl() {
-        preloaderActivate();
+        preloaderDisable();
 
         $node_count_container.hide();
 
@@ -74,11 +87,29 @@ $(document).ready(function () {
 
 
         $data_source_handler.val(data_source);
+        $graph_type_handler.val(graph_type);
 
         d3.json(url, function (er, cl) {
-            data = cl;
+
             data_loaded = true;
-            graphBuild();
+
+            switch (graph_type) {
+                case 'hierarchy':
+                    data = cl;
+                    graphBuild1();
+                    break;
+                case 'stratify':
+                    if (!checkOneRoot(cl)) {
+                        cl = addOneRoot(cl);
+                    }
+
+                    data = convertJsonData(cl);
+                    links_data = convertJsonDataLinks(cl);
+                    graphBuild2();
+                    break;
+            }
+            preloaderDisable();
+
         });
 
     }
@@ -102,7 +133,7 @@ $(document).ready(function () {
                 d3.select('#value-simple').text(d3.format('.2')(val));
                 beta_value = val;
                 localStorage.setItem('beta_value', beta_value);
-                graphBuild();
+                getDataUrl();
             });
 
         let gSimple = d3
@@ -128,10 +159,6 @@ $(document).ready(function () {
         $full_preloader.hide();
     }
 
-    function graphBuild() {
-        graphBuild1();
-        preloaderDisable();
-    }
 
     function graphBuild1() {
         if (!data_loaded) {
@@ -147,12 +174,10 @@ $(document).ready(function () {
             })
             .curve(d3.curveBundle.beta(beta_value));
 
-        let svg = d3.select("#graph_svg");
-
+        svg = d3.select("#graph_svg");
         svg.select("#first").remove();
 
         width = svg.node().getBoundingClientRect().width / 2;
-
         height = svg.node().getBoundingClientRect().height / 2;
 
         let zoomable_layer = svg.append('g')
@@ -168,7 +193,6 @@ $(document).ready(function () {
             'transform', "translate(" + width + "," + height + ")"
         );
 
-
         Zoom(x, y, k);
 
         zoom = d3.zoom().scaleExtent([-Infinity, Infinity]).on('zoom', function () {
@@ -183,15 +207,11 @@ $(document).ready(function () {
 
         svg.call(zoom);
 
-
         let root = d3.hierarchy(packageHierarchy(data), (d) => d.children);
         cluster(root);
 
         let nodes = root.descendants();
-
-
         let links = packageImports(nodes);
-        console.log(links);
 
 
         link = link
@@ -384,7 +404,7 @@ $(document).ready(function () {
             nodes.forEach(function (d) {
                 if (d.data.imports) {
                     d.data.imports.forEach(function (i) {
-                        imports.push({source: map[d.data.name], target: map[i], path: map[d.data.name].path(map[i])}   );
+                        imports.push({source: map[d.data.name], target: map[i], path: map[d.data.name].path(map[i])});
                     });
                 }
             });
@@ -392,5 +412,229 @@ $(document).ready(function () {
             return imports;
         }
     }
+
+
+    function addOneRoot(p_data) {
+        for (let i = 0; i < p_data.length; i++) {
+            p_data[i].name = 'flare.' + p_data[i].name;
+            for (let j = 0; j < p_data[i].imports.length; j++) {
+                p_data[i].imports[j] = 'flare.' + p_data[i].imports[j];
+            }
+        }
+        return p_data;
+    }
+
+    function checkOneRoot(p_data) {
+        let root_str = p_data[0].name.substr(0, p_data[0].name.indexOf('.'));
+
+        for (let i = 0; i < p_data.length / 2; i++) {
+            if (p_data[i].name.indexOf(root_str) > -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function convertJsonData(json) {
+        let converted = [];
+        let temp_ids = [];
+
+        for (let i = 0; i < json.length; i++) {
+            let split_names = json[i].name.split('.');
+
+            let split_rubrics;
+
+            if (json[i].rubric !== undefined) {
+                split_rubrics = json[i].rubric.split('.');
+            }
+            else {
+                split_rubrics = split_names;
+            }
+
+            let temp = '';
+
+            for (let j = 0; j < split_names.length; j++) {
+                if (j === 0) {
+                    temp += split_names[j];
+                }
+                else {
+                    temp += '.' + split_names[j];
+                }
+
+                if (temp_ids.includes(temp)) {
+                    continue;
+                }
+                else {
+                    temp_ids.push(temp);
+                }
+
+                let value = '';
+                let title = '';
+
+                if (j == split_names.length - 1) {
+
+                    if (json[i].size == undefined) {
+                        json[i].size = json[i].imports.length ;
+                    }
+                    value = json[i].size + '';
+
+                    title = json[i].title;
+                }
+                else {
+                    title = split_rubrics[j - 1];
+                }
+
+                let array = {
+                    id: temp,
+                    value: value,
+                    title: title
+                };
+                converted.push(array);
+            }
+        }
+
+        return converted;
+    }
+
+
+    function convertJsonDataLinks(json) {
+        let converted = [];
+
+        for (let i = 0; i < json.length; i++) {
+
+            for (let j = 0; j < json[i].imports.length; j++) {
+                const array = {
+                    source: json[i].name,
+                    target: json[i].imports[j]
+                };
+                converted.push(array);
+            }
+        }
+        return converted;
+    }
+
+    function graphBuild2() {
+        let bubble_layer, h, height, line, pack, stratify, svg, vis, w, width, zoom, zoomable_layer;
+        let bubbles, enb, index, links, root;
+
+
+        svg = d3.select("#graph_svg");
+        svg.select("#first").remove();
+
+        width = svg.node().getBoundingClientRect().width;
+
+        height = svg.node().getBoundingClientRect().height;
+
+        zoomable_layer = svg.append('g').attr('id', 'first');
+
+        zoom = d3.zoom().scaleExtent([-Infinity, Infinity]).on('zoom', function () {
+            return zoomable_layer.attrs({
+                transform: d3.event.transform
+            });
+        });
+
+        svg.call(zoom);
+
+
+        vis = zoomable_layer.append('g').attrs({
+            transform: "translate(" + (width / 2) + "," + (height / 2) + ")"
+        });
+
+        stratify = d3.stratify().parentId(function (d) {
+            return d.id.substring(0, d.id.lastIndexOf("."));
+        });
+
+        w = width - 8;
+
+        h = height - 8;
+
+        pack = d3.pack().size([w, h]).padding(3);
+
+        line = d3.line()
+            .curve(d3.curveBundle.beta(beta_value))
+
+            .x(function (d) {
+                return d.x;
+            })
+            .y(function (d) {
+                return d.y;
+            });
+
+        let betaf = 50;
+        let beta = betaf / 100;
+
+
+        bubble_layer = vis.append('g').attrs({
+            transform: "translate(" + (-w / 2) + "," + (-h / 2) + ")"
+        });
+
+
+        root1 = stratify(data);
+
+
+        root2 = root1.sum(function (d) {
+            return d.value;
+        });
+
+
+        root = root2.sort(function (a, b) {
+            return d3.descending(a.value, b.value);
+        });
+
+
+        pack(root);
+        index = {};
+        root.eachBefore(function (d) {
+            index[d.data.id] = d;
+            return index[d.data.id];
+        });
+
+        links_data.forEach(function (d) {
+            d.source = index[d.source];
+            d.target = index[d.target];
+            d.path = d.source.path(d.target);
+            return d.path;
+        });
+
+
+        bubbles = bubble_layer.selectAll('.bubble').data(root.descendants());
+
+        enb = bubbles.enter().append('circle').attrs({
+            "class": 'bubble',
+            cx: function (d) {
+                return d.x;
+            },
+            cy: function (d) {
+                return d.y;
+            },
+            r: function (d) {
+                return d.r;
+            }
+        });
+        enb.append('title').text(function (d) {
+            return d.data.title || d.id.substring(d.id.lastIndexOf(".") + 1).split(/(?=[A-Z][^A-Z])/g).join(' ');
+        });
+        links = bubble_layer.selectAll('.link').data(links_data);
+
+        svg.on('click', function (d) {
+            betaf += 10;
+            beta = betaf / 100;
+            links.each(function (d1) {
+                d1.classed("link--target", false)
+                    .classed("link--source", true)
+            })
+
+            // bubble_layer.selectAll('.link').enter().curve(d3.curveBundle.beta(beta));
+        });
+
+
+        return links.enter().append('path').attrs({
+            "class": 'link',
+            d: function (d) {
+                return line(d.path);
+            }
+        });
+    }
+
 
 });
