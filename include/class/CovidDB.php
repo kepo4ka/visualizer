490fc4f64d2a3f09561bf23ce582b609c3062185
kepo4ka <konstantin.bugulov@mail.ru>
2020-04-24 15:32:06 +0300

    Update CovidDB.php

--- include/class/CovidDB.php
+++ include/class/CovidDB.php
@@ -11,10 +11,10 @@ class CovidDB
     var $destinations = 'destinations';
     var $visa = 'visa';
     var $covid = 'covid';
-    var $length = 100;
+    var $length;
 
 
-    function __construct($length = 100)
+    function __construct($length = 0)
     {
         global $db;
         $this->db = $db;
@@ -24,26 +24,30 @@ class CovidDB
 
     function getAirports()
     {
+        $query = "select id, name, city_id, city_name from airports";
 
-        $query = "select id, name, city_id, city_name from airports LIMIT {$this->length}";
+        if (!empty($this->length)) {
+            $query .= " LIMIT {$this->length}";
+        }
+
+
         $airports = $this->db->getAll($query);
 
 
         foreach ($airports as &$airport) {
             $airport['title'] = $airport['name'];
-            $airport['title1'] = $airport['city_name'];
+
+            $airport['name'] = $airport['id'];
 
             $query = "select countries.name, countries.continent FROM cities, countries, airports
             where airports.id={$airport['id']} AND 
             airports.city_id=cities.id AND
-            cities.country_iso=countries.iso";
+            cities.country_iso=countries.iso
+            AND 1";
 
             $airport['country'] = $this->db->getRow($query);
 
-            if (empty($airport['country']))
-            {
-                \Helper\Helper::echoVarDumpPre($query);
-            }
+            $airport['title1'] = $airport['city_name'] . ' > ' . $airport['country']['name'] . ' > ' . $airport['country']['continent'];
 
             $airport['rubric'] = "{$airport['country']['continent']}.{$airport['country']['name']}.{$airport['city_name']}";
 //            $airport['rubric_md5'] = ' .' . splitMd5($airport['rubric']);
@@ -52,11 +56,38 @@ class CovidDB
             $query = "select `air_to` from {$this->destinations} WHERE air_from={$airport['id']}";
             $airport['destinations'] = $this->db->getCol($query);
             $airport['imports'] = $airport['destinations'];
+            unset($airport['destinations']);
+            unset($airport['country']);
+            unset($airport['id']);
         }
         unset($airport);
 
+
         $airports = clearEmptyReferences($airports, PRIMARY_FIELD, REFEREFCES_FIELD);
 
+
+        foreach ($airports as &$airport) {
+            $airport['rubric_md5'] = ' .' . splitMd5($airport['rubric']);
+            $airport['name'] = $airport['rubric_md5'] . $airport['name'];
+
+            foreach ($airport['imports'] as &$destination) {
+
+                $query = "select countries.name as country_name, cities.name as city_name, countries.continent FROM cities, countries, airports
+                    where airports.id={$destination} AND 
+                    airports.city_id=cities.id AND
+                    cities.country_iso=countries.iso";
+
+                $country_info = $this->db->getRow($query);
+
+                $rubric = "{$country_info['continent']}.{$country_info['country_name']}.{$country_info['city_name']}";
+                $rubric_md5 = ' .' . splitMd5($rubric);
+                $destination = $rubric_md5 . $destination;
+            }
+            unset($destination);
+//            unset($airport['city_name']);
+//            unset($airport['city_id']);
+        }
+        unset($airport);
 
 
         return $airports;
